6470c00304232b97a8d19bff41fefc20f6bdb7bb
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-01-12 15:43:26 +0200

    adaug thunk pt crearea quizzurilor

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,4 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { addQuizzId } from "../topics/topicsSlice";
+
+export const createQuizz = (payload) => {
+    return (dispatch) => {
+        dispatch(addQuizz(payload));
+        const quizzIdPayload = {
+            quizzId: payload.id,
+            topicId: payload.topicId
+        }
+        dispatch(addQuizzId(quizzIdPayload));
+    }
+}
 
 const slice = createSlice({
     name: 'quizzes',
