d80f0c73fd07a8bafd6fe491c3b7165d06b437aa
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-01-12 12:49:24 +0200

    adaug functionalitate creare topic

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -3,11 +3,14 @@ import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
+import { addTopic } from "../features/topics/topicsSlice";
+import { useDispatch } from "react-redux";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +18,12 @@ export default function NewTopicForm() {
       return;
     }
 
+    dispatch(addTopic({
+      id: uuidv4(),
+      name: name,
+      icon: icon,
+      quizIds: []
+    }));
     // dispatch your add topic action here
     history.push(ROUTES.topicsRoute());
   };
